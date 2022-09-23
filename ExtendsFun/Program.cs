8d187dd817895b98fdfb25f62102d6395d00aa36
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-09-23 16:19:25 +0800

    [feat]: Realized extends function to remove duplication of list

---
+++ ExtendsFun/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace ExtendsFun;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> listStr = new List<string>
+        {
+            "hello",
+            "world",
+            "hello",
+            "world"
+        };
+
+        List<int> listInt = new List<int> { 1, 2, 2, 3, 3, 4, 5 };
+        
+        
+        var resultStr = listStr.RemoveDuplication();
+        var resultInt = listInt.RemoveDuplication();
+        
+        Console.WriteLine(string.Join(" ",resultStr.ToArray()));
+        Console.WriteLine(string.Join(" ",resultInt.ToArray()));
+    }
+}
